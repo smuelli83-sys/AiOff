3d4a2946ffb5545f20865d93a00f868f6284a3af
smuelli83-sys <s.muelli83@googlemail.com>
2026-08-06 07:04:08 +0200

    Update and rename types.py to offlineai_core/types.py

--- offlineai_core/types.py
+++ offlineai_core/types.py
@@ -2,13 +2,8 @@ from enum import Enum
 
 
 class ProviderType(str, Enum):
-
     CORE = "core"
-
     AI = "ai"
-
     INFRASTRUCTURE = "infrastructure"
-
     SECURITY = "security"
-
     EXTENSION = "extension"
