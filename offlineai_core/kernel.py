657f6383af1d9bbfa2d18d6f348ecdad91c1397a
smuelli83-sys <s.muelli83@googlemail.com>
2026-08-06 06:49:44 +0200

    Create kernel.py

---
+++ offlineai_core/kernel.py
@@ -0,0 +1,17 @@
+from offlineai_core.context import ApplicationContext
+
+
+class Kernel:
+
+    def __init__(self) -> None:
+        self.context = ApplicationContext()
+
+    def start(self) -> None:
+
+        self.context.logger.info("Starting OfflineAI Enterprise")
+
+        self.context.logger.info("Loading Configuration")
+
+        self.context.logger.info("Loading Registry")
+
+        self.context.logger.info("Kernel Ready")
