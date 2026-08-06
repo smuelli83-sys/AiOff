df9a6f415894afd52366123ce5d03f4df9f8f026
smuelli83-sys <s.muelli83@googlemail.com>
2026-08-06 06:53:33 +0200

    Update context.py

--- offlineai_core/context.py
+++ offlineai_core/context.py
@@ -6,3 +6,14 @@ class ApplicationContext:
     def __init__(self):
 
         self.logger = Logger()
+
+from offlineai_core.configuration import Configuration
+from offlineai_core.logger import Logger
+
+
+class ApplicationContext:
+
+    def __init__(self):
+
+        self.logger = Logger()
+        self.configuration = Configuration()
